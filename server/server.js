d117f010bba0877fa0d17c5959dd928a47a08666
ShirLicht <licht.shir@gmail.com>
2019-07-29 17:15:37 +0300

    Added socket.io events :  user logs in + new message

--- server/server.js
+++ server/server.js
@@ -3,17 +3,39 @@ const http = require("http").Server(app);
 const io = require("socket.io")(http);
 
 let users = [];
-let message = [];
+let messages = [];
 let index = 0;
 
 io.on("connection", socket => {
  
+    socket.emit('loggedIn', {
+        users: users.map(s => s.username),
+        messages: messages
+    });
     socket.on('newuser', username => {
         console.log(`${username} has arrived at the party.`);
         socket.username = username;
         users.push(socket);
+
+        //pass back a username - so everyone will know there is a new user that is online
+        io.emit('userOnilne', socket.username);
     });
   
+    //Get new message
+    socket.on('msg', msg => {
+        let message = {
+            index : index,
+            username: socket.username,
+            msg: msg
+        }
+
+        messages.push(message);
+
+        io.emit('msg', message);
+
+        index++;
+    });
+
  //Disconnect
   socket.on("disconnect", () => {
       console.log(`${socket.username} has left the party.`)

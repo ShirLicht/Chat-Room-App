50824077e3e8469a59ab83c6b18d28c2b675e922
ShirLicht <licht.shir@gmail.com>
2019-07-30 12:21:21 +0300

    Updated the App.vue file

--- client/client/src/App.vue
+++ client/client/src/App.vue
@@ -1,24 +1,48 @@
 <template>
   <div id="app">
+      <div class="header">
+            <h1>Chat Room</h1>
+            <p class="username">Username: {{username}}</p>
+            <p class="online">Online Users: {{users.length}}</p>
+            
+      </div>
   </div>
 </template>
 
 <script>
+import io from 'socket.io-client';
 
 export default {
   name: 'app',
   components: {
+  },
+  data: function(){
+    return{
+       username:"",
+       //connect to the server
+       socket: io("http://localhost:3000"),
+       messages: [],
+       users: []
+    }
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+body{
+  font-family: 'Avenir', Arial, Helvetica, sans-serif;
+  color: #2C3E50;
+  margin: 0;
+  padding: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display:flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100%;
+  //tablet size
+  max-width: 768px;
+  margin: 0 auto;
+
 }
 </style>
